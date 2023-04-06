e3c20fd9491d553892a032740f6e45a8b14de0ab
HappyRashair <przemyslaw.wiczolek@happyteam.io>
2023-04-06 07:56:23 +0200

    Initial repo

---
+++ MagickNet/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Diagnostics;
+using ImageMagick;
+
+Console.WriteLine("Hello, Magic.Net!");
+
+var st = Stopwatch.StartNew();
+for (int i = 0; i < 1000; ++i)
+{
+    using (var image = new MagickImage("image.png"))
+    {
+        var size = new MagickGeometry(100, 100);
+        // This will resize the image to a fixed size without maintaining the aspect ratio.
+        // Normally an image will be resized to fit inside the specified size.
+        size.IgnoreAspectRatio = true;
+
+        image.Resize(size);
+
+        // Save the result
+        image.Write($"image-resized{i}.png");
+    }
+}
+Console.WriteLine($"Processing took {st.Elapsed.TotalSeconds}");
---
+++ SkiaSharpTest/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Diagnostics;
+using SkiaSharp;
+
+Console.WriteLine("Hello, SkiaSharp!");
+
+var st = Stopwatch.StartNew();
+for (int i = 0; i < 1000; ++i)
+{
+    var fileContents = File.ReadAllBytes("image.png");
+
+    using MemoryStream ms = new MemoryStream(fileContents);
+    using SKBitmap sourceBitmap = SKBitmap.Decode(ms);
+
+    int height = Math.Min(100, sourceBitmap.Height);
+    int width = Math.Min(100, sourceBitmap.Width);
+
+    using SKBitmap scaledBitmap = sourceBitmap.Resize(new SKImageInfo(width, height), SKFilterQuality.High);
+    using SKImage scaledImage = SKImage.FromBitmap(scaledBitmap);
+    using SKData data = scaledImage.Encode();
+
+    File.WriteAllBytes($"image-resized-${i}.png", data.ToArray());
+}
+Console.WriteLine($"Processing took {st.Elapsed.TotalSeconds}");
